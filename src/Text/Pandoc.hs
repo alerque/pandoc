1beda3a05375b389197a50e91060a9cb2d005690
Caleb Maclennan <caleb@alerque.com>
2015-08-03 12:22:02 +0300

    Add support for Sile as an output format

--- src/Text/Pandoc.hs
+++ src/Text/Pandoc.hs
@@ -93,6 +93,7 @@ module Text.Pandoc
                , writeMarkdown
                , writePlain
                , writeRST
+               , writeSile
                , writeLaTeX
                , writeConTeXt
                , writeTexinfo
@@ -151,6 +152,7 @@ import Text.Pandoc.Readers.EPUB
 import Text.Pandoc.Writers.Native
 import Text.Pandoc.Writers.Markdown
 import Text.Pandoc.Writers.RST
+import Text.Pandoc.Writers.Sile
 import Text.Pandoc.Writers.LaTeX
 import Text.Pandoc.Writers.ConTeXt
 import Text.Pandoc.Writers.Texinfo
@@ -299,6 +301,7 @@ writers = [
      writeDocbook o{ writerDocbook5 = True })
   ,("opml"         , PureStringWriter writeOPML)
   ,("opendocument" , PureStringWriter writeOpenDocument)
+  ,("sile"         , PureStringWriter writeSile)
   ,("latex"        , PureStringWriter writeLaTeX)
   ,("beamer"       , PureStringWriter $ \o ->
      writeLaTeX o{ writerBeamer = True })
