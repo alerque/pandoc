d9d31e1fa6f736df1dc54f3ac08b2980c0c5a6c8
Caleb Maclennan <caleb@alerque.com>
2015-11-12 18:13:03 +0200

    Handle raw strings and embeding in other formats much like LaTeX

--- src/Text/Pandoc/Writers/Markdown.hs
+++ src/Text/Pandoc/Writers/Markdown.hs
@@ -471,6 +471,12 @@ blockToMarkdown' opts b@(RawBlock f str) = do
                     text str <> text "\n"
                 | isEnabled Ext_raw_attribute opts -> rawAttribBlock
                 | otherwise -> renderEmpty
+      | f `elem` ["sile", "sil"] ->
+            case () of
+              _ | isEnabled Ext_raw_sile opts -> return $
+                    text str <> text "\n"
+                | isEnabled Ext_raw_attribute opts -> rawAttribBlock
+                | otherwise -> renderEmpty
       | isEnabled Ext_raw_attribute opts -> rawAttribBlock
       | otherwise -> renderEmpty
 blockToMarkdown' opts HorizontalRule = do
@@ -1143,6 +1149,11 @@ inlineToMarkdown opts il@(RawInline f str) = do
       | f `elem` ["latex", "tex"] ->
             case () of
               _ | isEnabled Ext_raw_tex opts -> return $ text str
+                | isEnabled Ext_raw_attribute opts -> rawAttribInline
+                | otherwise -> renderEmpty
+      | f `elem` ["sile", "sil"] ->
+            case () of
+              _ | isEnabled Ext_raw_sile opts -> return $ text str
                 | isEnabled Ext_raw_attribute opts -> rawAttribInline
                 | otherwise -> renderEmpty
       | isEnabled Ext_raw_attribute opts -> rawAttribInline
