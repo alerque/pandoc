07b2345a6f8cee4880704c8c923f9084281289f7
Caleb Maclennan <caleb@alerque.com>
2015-11-16 20:59:30 +0200

    Fix bothed markdown reader

--- src/Text/Pandoc/Readers/Markdown.hs
+++ src/Text/Pandoc/Readers/Markdown.hs
@@ -1064,8 +1064,6 @@ rawTeXBlock = do
                   rawLaTeXBlock `sepEndBy1` blankline)
         <|> (B.rawBlock "context" . concat <$>
                   rawConTeXtEnvironment `sepEndBy1` blankline)
-        <|> (B.rawBlock "sile" . concat <$>
-                  rawSileEnvironment `sepEndBy1` blankline)
   spaces
   return $ return result
 
