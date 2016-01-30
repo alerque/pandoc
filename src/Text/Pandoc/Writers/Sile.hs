1015ede8dfd2f099acd45ba5f1083a6a7ef7406c
Caleb Maclennan <caleb@alerque.com>
2016-01-30 13:31:33 +0200

    Revert "Work around SILE bug (https://github.com/simoncozens/sile/issues/254)"
    This reverts commit 3eb29bd4db72b5a3cb5404fb5f7cee08f6f28b81.
    
    Fixed upstream

--- src/Text/Pandoc/Writers/Sile.hs
+++ src/Text/Pandoc/Writers/Sile.hs
@@ -691,10 +691,7 @@ inlineToSile (RawInline f x)
   | f == Format "latex" || f == Format "tex" || f == Format "sile" || f == Format "sil"
                         = return $ text x
   | otherwise           = return empty
--- The extra \relax{} appended here is to work around this bug:
--- https://github.com/simoncozens/sile/issues/254
--- and should be removed whet it's fixed
-inlineToSile (LineBreak) = return $ "\\break" <> cr <> "\\relax{}"
+inlineToSile (LineBreak) = return $ "\\break" <> cr
 inlineToSile Space = return space
 inlineToSile (Link txt ('#':ident, _)) = do
   contents <- inlineListToSile txt
