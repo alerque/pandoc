61fa249679287328a52f116e434f88a68996e4d9
Caleb Maclennan <caleb@alerque.com>
2016-12-10 19:05:31 +0300

    Setup basic external link processing

--- src/Text/Pandoc/Writers/Sile.hs
+++ src/Text/Pandoc/Writers/Sile.hs
@@ -623,7 +623,7 @@ inlineToSile (Link _ txt (src, _)) =
                    braces ("\\url" <> braces contents)
         _ -> do contents <- inlineListToSile txt
                 src' <- stringToSile URLString (escapeURI src)
-                return $ text ("\\href{" ++ src' ++ "}{") <>
+                return $ text ("\\anchor[href=" ++ src' ++ "]{") <>
                          contents <> char '}'
 inlineToSile (Image attr _ (source, _)) = do
   modify $ \s -> s{ stGraphics = True }
