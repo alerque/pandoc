f19dd52d69a4b4675b947c7995b6e45513d91d7e
Caleb Maclennan <caleb@alerque.com>
2019-02-19 18:09:18 +0300

    Compare to LaTeX on 2.6

--- src/Text/Pandoc/Writers/Sile.hs
+++ src/Text/Pandoc/Writers/Sile.hs
@@ -1,8 +1,9 @@
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE PatternGuards       #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-
-Copyright (C) 2006-2017 John MacFarlane <jgm@berkeley.edu>
+Copyright (C) 2006-2018 John MacFarlane <jgm@berkeley.edu>
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -21,7 +22,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 {- |
    Module      : Text.Pandoc.Writers.Sile
-   Copyright   : Copyright (C) 2015-2017 Caleb Maclennan
+   Copyright   : Copyright (C) 2015-2018 Caleb Maclennan
    License     : GNU GPL, version 2 or above
 
    Maintainer  : Caleb Maclennan <caleb@alerque.com>
